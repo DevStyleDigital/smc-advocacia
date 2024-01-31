228f1781f6dcde6f3498d9ad575a548aa17c87cd
Guilherme Brogio <guilhermebrogio.ps@gmail.com>
2024-01-31 09:42:55 -0300

    fix: whatsapp link

--- src/components/cta.tsx
+++ src/components/cta.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { ArrowRight } from "lucide-react";
 import { Button } from "./ui/button";
 
@@ -12,16 +13,19 @@ export const CTA = ({
 			size="lg"
 			className="w-fit group transition-all relative overflow-hidden sm:px-12 py-8"
 			onClick={() => {
-				window.open(`https://wa.me/15991198369?text=${encodeURIComponent(
-					"Olá vim do site!",
-				)}`, '_blank')
+				window.open(
+					`https://wa.me/15991198369?text=${encodeURIComponent(
+						"Olá vim do site!",
+					)}`,
+					"_blank",
+				);
 			}}
 		>
-				<ArrowRight className="group-hover:translate-x-0 mr-2 w-0 group-hover:opacity-100 opacity-0 -translate-x-8 group-hover:w-6 h-6 transition-all" />
-				<span className="translate-x-0 transition-all [font-variant:small-caps] text-xl whitespace-normal text-center">
-					Fale com um especialista
-				</span>
-				<ArrowRight className="group-hover:translate-x-8 ml-2 group-hover:w-0 group-hover:opacity-0 opacity-100 translate-x-0 w-6 h-6 transition-all" />
+			<ArrowRight className="group-hover:translate-x-0 mr-2 w-0 group-hover:opacity-100 opacity-0 -translate-x-8 group-hover:w-6 h-6 transition-all" />
+			<span className="translate-x-0 transition-all [font-variant:small-caps] text-xl whitespace-normal text-center">
+				Fale com um especialista
+			</span>
+			<ArrowRight className="group-hover:translate-x-8 ml-2 group-hover:w-0 group-hover:opacity-0 opacity-100 translate-x-0 w-6 h-6 transition-all" />
 		</Button>
 	);
 };
