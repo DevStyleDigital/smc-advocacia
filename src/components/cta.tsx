6b75de393d9c086d49eebc075d6f46c71bd7cb78
Guilherme Brogio <guilhermebrogio.ps@gmail.com>
2024-01-31 09:30:01 -0300

    fix: whatsapp link

--- src/components/cta.tsx
+++ src/components/cta.tsx
@@ -1,5 +1,4 @@
 import { ArrowRight } from "lucide-react";
-import Link from "next/link";
 import { Button } from "./ui/button";
 
 export const CTA = ({
@@ -10,21 +9,19 @@ export const CTA = ({
 	return (
 		<Button
 			variant={variant}
-			asChild
 			size="lg"
 			className="w-fit group transition-all relative overflow-hidden sm:px-12 py-8"
-		>
-			<Link
-				href={`https://wa.me/15991198369?text=${encodeURIComponent(
+			onClick={() => {
+				window.open(`https://wa.me/15991198369?text=${encodeURIComponent(
 					"Olá vim do site!",
-				)}`}
-			>
+				)}`, '_blank')
+			}}
+		>
 				<ArrowRight className="group-hover:translate-x-0 mr-2 w-0 group-hover:opacity-100 opacity-0 -translate-x-8 group-hover:w-6 h-6 transition-all" />
 				<span className="translate-x-0 transition-all [font-variant:small-caps] text-xl whitespace-normal text-center">
 					Fale com um especialista
 				</span>
 				<ArrowRight className="group-hover:translate-x-8 ml-2 group-hover:w-0 group-hover:opacity-0 opacity-100 translate-x-0 w-6 h-6 transition-all" />
-			</Link>
 		</Button>
 	);
 };
